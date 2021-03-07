40de5ec24d6f9cc5debe396a812f95799843467e
hareesh <hareesh996@gmail.com>
2021-03-07 12:51:33 +0530

    initial commit

---
+++ apps/shopping-cart/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shopping-ui-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'shopping-cart';
+}
